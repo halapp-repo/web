fa8407ee47d59d48383a89c2723d93a53035c69e
Furkan Ozdeslik <furkan.ozdeslik@gmail.com>
2022-10-04 00:15:13 +0300

    update code

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,29 +6,34 @@ import { useEffect } from 'react';
 import { useAppDispatch } from '../../store/hooks';
 import { fetchInventories } from '../../store/inventories/inventoriesSlice';
 import * as moment from 'moment-timezone';
-import { updateSelectedDate } from '../../store/ui/uiSlice';
+import { updateListingSelectedDate } from '../../store/ui/uiSlice';
 import Contact from './Contact';
+import PriceFilter from './PriceFilter';
 
 const DashboardDefault = () => {
   const dispatch = useAppDispatch();
 
   // Fetch inital Data
   useEffect(() => {
-    dispatch(updateSelectedDate(moment.tz('Europe/Istanbul').format('YYYY-MM-DD')));
+    dispatch(updateListingSelectedDate(moment.tz('Europe/Istanbul').format('YYYY-MM-DD')));
     dispatch(fetchInventories());
   }, []);
 
   return (
     <Grid container rowSpacing={4.5} justifyContent="right" columnSpacing={2.75} alignItems="right">
-      <Grid item xs={12} sm={6} md={8} lg={6}>
+      <Grid item xs={12} sm={3} md={3} lg={3}>
+        <MainCard sx={{ mt: 2 }}>
+          <PriceFilter />
+        </MainCard>
+      </Grid>
+      <Grid item xs={12} sm={6} md={6} lg={6}>
         <MainCard sx={{ mt: 2 }}>
           <PriceTable />
         </MainCard>
       </Grid>
-      <Grid item xs={12} sm={4} md={3} lg={4}>
+      <Grid item xs={12} sm={3} md={3} lg={3}>
         <MainCard sx={{ mt: 2 }}>
-          {' '}
-          <Contact />{' '}
+          <Contact />
         </MainCard>
       </Grid>
     </Grid>
--- src/store/ui/uiSlice.ts
+++ src/store/ui/uiSlice.ts
@@ -2,24 +2,43 @@ import { createSelector, createSlice } from '@reduxjs/toolkit';
 import type { RootState } from '../index';
 import { UIState } from './uiState';
 import type { PayloadAction } from '@reduxjs/toolkit';
+import { City } from '../../models/city';
 
 const initialState = {
-  prices: {}
+  listing: {
+    filteredProductName: '',
+    selectedCity: City.istanbul
+  }
 } as UIState;
 
 const UISlice = createSlice({
   name: 'ui',
   initialState,
   reducers: {
-    updateSelectedDate: (state: UIState, action: PayloadAction<string>) => {
-      state.prices.selectedDate = action.payload;
+    updateListingSelectedDate: (state: UIState, action: PayloadAction<string>) => {
+      state.listing.selectedDate = action.payload;
+    },
+    updateListingProductNameFilter: (state: UIState, action: PayloadAction<string>) => {
+      state.listing.filteredProductName = action.payload.toLowerCase();
     }
   }
 });
 
-export const { updateSelectedDate } = UISlice.actions;
-export const selectUIPricesSelectedDate = createSelector(
+export const { updateListingSelectedDate, updateListingProductNameFilter } = UISlice.actions;
+export const selectUIListingSelectedDate = createSelector(
   (state: RootState) => state.ui,
-  (state: UIState) => state.prices.selectedDate
+  (state: UIState) => state.listing.selectedDate
+);
+export const selectUIListingSelectedCategory = createSelector(
+  (state: RootState) => state.ui,
+  (state: UIState) => state.listing.selectedCategory
+);
+export const selectUIListingProductNameFilter = createSelector(
+  (state: RootState) => state.ui,
+  (state: UIState) => state.listing.filteredProductName
+);
+export const selectUIListingSelectedCity = createSelector(
+  (state: RootState) => state.ui,
+  (state: UIState) => state.listing.selectedCity
 );
 export default UISlice.reducer;
