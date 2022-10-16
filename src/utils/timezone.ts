55c596c94fbd564a277f1c5b852f6fe3a39e0f88
Furkan Ozdeslik <furkan.ozdeslik@gmail.com>
2022-10-16 23:08:25 +0300

    Update timezone.ts

--- src/utils/timezone.ts
+++ src/utils/timezone.ts
@@ -1,4 +1,5 @@
 import moment from 'moment';
 import('moment-timezone');
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const trMoment = (...args: any) => moment(...args).tz('Europe/Istanbul');
