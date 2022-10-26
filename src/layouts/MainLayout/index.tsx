fe864e4992d5ea9f528862d9b51d9fdda88ef2ea
Furkan Ozdeslik <furkan.ozdeslik@gmail.com>
2022-10-26 19:45:35 +0300

    add footer

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,18 +1,27 @@
 import { Box, Toolbar } from '@mui/material';
 import { Outlet } from 'react-router-dom';
-import Header from './header/index';
+import Header from './header';
 import LayoutInitializer from '../../components/LayoutInitializer';
+import Footer from './footer';
 
 const MainLayout = () => {
   return (
-    <Box sx={{ display: 'flex', width: '100%' }}>
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        minHeight: '100vh',
+        margin: 0,
+        flexDirection: 'column'
+      }}>
       <Header />
-      <Box component="main" sx={{ width: '100%', flexGrow: 1, p: { xs: 2, sm: 3 } }}>
+      <Box component="main" sx={{ width: '100%', flex: 1, flexGrow: 1, p: { xs: 2, sm: 3 } }}>
         <Toolbar />
         <LayoutInitializer>
           <Outlet />
         </LayoutInitializer>
       </Box>
+      <Footer />
     </Box>
   );
 };
